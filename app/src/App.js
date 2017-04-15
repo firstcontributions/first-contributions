b022a883806fa53dbdb9928ff8c0f0e68f7c221e
Christian <cswcsdcs@gmail.com>
2017-04-14 23:04:53 -0600

    Fixed some css stuff, I am passing down an object which I then access the different properties

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import LinkButton from './components/LinkButton';
+import Card from './components/Card';
+
+const ReactOS = {
+  title: "React",
+  summary: "Javascript library for redering UI",
+  link: "Github page",
+  site: "https://facebook.github.io/react/",
+  src: "https://s3.amazonaws.com/media-p.slid.es/uploads/espenhovlandsdal/images/566501/react-logo-colored.png"
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-        <h1>Make your first open source contribution in 5 minutes</h1>
+      <div>
+        <div className="App">
+          <div className="App-header">
+          <h1>Make your first open source contribution in 5 minutes</h1>
+          </div>
+          <LinkButton />
         </div>
-      <LinkButton />
+        <Card Info={ReactOS}/>
       </div>
     );
   }
--- app/src/components/Card.jsx
+++ app/src/components/Card.jsx
@@ -3,7 +3,10 @@ import './Card.css';
 
 const Card = (props) => (
     <div className="CardContainer" >
-        <h1 className="CardTitle">{props.Info.title}</h1>
+        <div className="Row">
+            <h1 className="CardTitle">{props.Info.title}</h1>
+            <img className="Image" src={props.Info.src}/>
+        </div>
         <p className="CardSummary">{props.Info.summary}</p>
         <a href={props.Info.site} className="CardLink">{props.Info.link}</a>
     </div>
