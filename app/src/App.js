f15f3fdf2dc1d0dcfc9384f50e206f72b001f16f
sergaben <gabense@hotmail.com>
2017-09-21 11:42:21 +0100

    Added ProjectList folder and Card feature

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-// import LinkButton from './components/LinkButton/LinkButton';
-// import Navbar from './components/Navbar/Navbar';
-//import Search from './components/Search/Search';
+import LinkButton from './components/LinkButton/LinkButton';
+import Navbar from './components/Navbar/Navbar';
 import CardsContainer from './components/ProjectList/ContainerForCards';
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+      <Navbar/>
+      <div className="App-header">
+      <h1>Make your first open source contribution in 5 minutes</h1>
+      </div>
+      <LinkButton/>
       <CardsContainer/>
       </div>
     );
