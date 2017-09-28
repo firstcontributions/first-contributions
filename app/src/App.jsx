c179f228610c2cc8d2654c51a9cc90f46965b8b1
Roshan Jossey <roshanjossey@gmail.com>
2017-09-29 02:31:29 +0530

    Fix linting issues in app code

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,16 +3,17 @@ import './App.css';
 import LinkButton from './components/LinkButton/LinkButton';
 import Navbar from './components/Navbar/Navbar';
 import CardsContainer from './components/ProjectList/CardsContainer';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="App-header">
-        <h1>Make your first open source contribution in 5 minutes</h1>
+          <h1>Make your first open source contribution in 5 minutes</h1>
         </div>
         <LinkButton />
-        <CardsContainer/>
+        <CardsContainer />
       </div>
     );
   }
