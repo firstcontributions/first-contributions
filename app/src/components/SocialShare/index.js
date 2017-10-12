d2ddccf83fd14f437d560f5b46991c683be5dd8c
GlantDeveloper <GlantDeveloper@users.noreply.github.com>
2017-10-12 14:39:15 -0700

    added tweet button to web app

---
+++ app/src/components/SocialShare/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TwitterCard from './twitter';
+import './SocialShare.css';
+
+const SocialShare = () => (
+  <section id="social-share">
+    <h3>Share your first contribution</h3>
+    <TwitterCard/>
+  </section>
+);
+
+export default SocialShare
