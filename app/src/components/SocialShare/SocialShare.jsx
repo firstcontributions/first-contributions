3a2f4db71d08544ab10aa4a8a016c9ed11625ec7
Roshan Jossey <roshanjossey@gmail.com>
2017-10-14 16:30:07 +0530

    Fix margin of link button

--- app/src/components/SocialShare/SocialShare.jsx
+++ app/src/components/SocialShare/SocialShare.jsx
@@ -3,7 +3,7 @@ import TwitterCard from './Twitter';
 import './SocialShare.css';
 
 const SocialShare = () => (
-  <section id="social-share">
+  <section id='social-share'>
     <h2>Share the news of your first contribution</h2>
     <TwitterCard/>
   </section>
