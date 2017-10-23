9399b6527831d0e2308d29c320fd76d391e04182
ubatin <mglwinkyawmyat@gmail.com>
2017-10-23 20:59:37 +0630

    Add necessary packages for .eslintrc

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -6,19 +6,19 @@ import CardsContainer from './components/ProjectList/CardsContainer';
 import SocialShare from './components/SocialShare/SocialShare';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Navbar />
-                <div className="App-header">
-                    <h1>Make your first open source contribution in 5 minutes</h1>
-                </div>
-                <LinkButton />
-                <CardsContainer />
-                <SocialShare/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="App-header">
+          <h1>Make your first open source contribution in 5 minutes</h1>
+        </div>
+        <LinkButton />
+        <CardsContainer />
+        <SocialShare />
+      </div>
+    );
+  }
 }
 
 export default App;
