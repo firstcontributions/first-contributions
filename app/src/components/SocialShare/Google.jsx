8d3c9fd07b68088a2186f8e6ea55dc72ec604c77
sridhar reddy <sridhar@yourdost.com>
2017-12-27 18:28:47 +0530

    changed github url to tutorial

--- app/src/components/SocialShare/Google.jsx
+++ app/src/components/SocialShare/Google.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Icon, IconPaths } from './icons.js';
 
-const GoogleLink = "https://plus.google.com/share?url=https://roshanjossey.github.io/first-contributions&prefilltext=I have made my first PR using this tutorial and I find it really simple and encouraging."
+const GoogleLink = "https://plus.google.com/share?url= https://goo.gl/66Axwe&prefilltext=I have made my first PR using this tutorial and I find it really simple and encouraging."
 
 const GoogleCard = () => (
-    <a className="icon-card " href={GoogleLink} target="_blank">
+    <a className="icon-card Google" href={GoogleLink} target="_blank">
         <Icon
             color="rgb(255, 255, 255)"
             size={50}
