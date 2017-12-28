eac825045b91506a43fd0164ec9413c42c48d6ff
Roshan Jossey <roshanjossey@gmail.com>
2017-12-28 14:10:02 +0530

    Fix share url title

--- app/src/components/SocialShare/Linkedin.jsx
+++ app/src/components/SocialShare/Linkedin.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Icon, IconPaths } from './icons.js';
 
-const LinkedinLink = "https://www.linkedin.com/shareArticle?mini=true&url=https://goo.gl/66Axwe&title=First%20Contributionsr%20&summary=A%20project%20to%20help%20beginners%20get%20started%20with%20contributing%20to%20open&20source&source=https://roshanjossey.github.io"
+const LinkedinLink = "https://www.linkedin.com/shareArticle?mini=true&url=https://goo.gl/66Axwe&title=First%20Contributions%20&summary=A%20project%20to%20help%20beginners%20get%20started%20with%20contributing%20to%20open&20source&source=https://roshanjossey.github.io"
 
 const LinkedinCard = () => (
     <a className="icon-card linkedin" href={LinkedinLink} target="_blank">
