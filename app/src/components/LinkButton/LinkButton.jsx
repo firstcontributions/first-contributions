80b764fad21c0bac2acbbb1bb06188f02028facb
Christian Nogueras <Christiannogueras@MBPdeChristian.lan>
2018-02-04 12:46:30 -0400

    Attempt to add Project to the website

--- app/src/components/LinkButton/LinkButton.jsx
+++ app/src/components/LinkButton/LinkButton.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 import './LinkButton.css';
+
+import Navbar from '../Navbar/Navbar';
 
 class LinkButton extends Component {
   render() {
     return (
-      <a className="LinkButton" href="https://github.com/Roshanjossey/first-contributions/blob/master/README.md">
-        <span> Get started </span>
-      </a>
+        <div className="Parent"> 
+          <a  className = "LinkButton" href="https://github.com/Roshanjossey/first-contributions/blob/master/README.md">
+            <span> Get started </span>
+          </a> 
+          <a  className = "LinkButton">
+            <span> Add a project </span>
+          </a> 
+        </div>   
     );
   }
 }
