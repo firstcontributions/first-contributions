86a0824cad4a7b38dfaa3a3c5017aa9135a5a5a7
Geetika19 <44170957+Geetika19@users.noreply.github.com>
2018-10-15 21:49:47 +0530

    Create square.c
    simple program to get square of number

---
+++ square.c
@@ -0,0 +1,15 @@
+ 
+#include<stdio.h>
+ 
+int main()
+{
+  int number, Square;
+ 
+  printf(" \n Please Enter any integer Value : ");
+  scanf("%d", &number);
+  
+  Square = number * number;
+  
+  printf("\n Square of a given number %d is  =  %d", number, Square);
+ 
+  return 0;
