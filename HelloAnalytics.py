cb7503eb0ef56757557c33ab011dcb3d6c374752
gagan <gagan.kumar@kiwitech.com>
2018-10-25 16:21:00 +0530

    HelloAnalytics python file added

---
+++ HelloAnalytics.py
@@ -0,0 +1,92 @@
+"""Hello Analytics Reporting API V4."""
+
+import argparse
+
+from apiclient.discovery import build
+import httplib2
+from oauth2client import client
+from oauth2client import file
+from oauth2client import tools
+
+SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
+DISCOVERY_URI = ('https://analyticsreporting.googleapis.com/$discovery/rest')
+CLIENT_SECRETS_PATH = 'client_secrets.json' # Path to client_secrets.json file.
+VIEW_ID = '92324711'
+
+
+def initialize_analyticsreporting():
+  """Initializes the analyticsreporting service object.
+
+  Returns:
+    analytics an authorized analyticsreporting service object.
+  """
+  # Parse command-line arguments.
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.RawDescriptionHelpFormatter,
+      parents=[tools.argparser])
+  flags = parser.parse_args([])
+
+  # Set up a Flow object to be used if we need to authenticate.
+  flow = client.flow_from_clientsecrets(
+      CLIENT_SECRETS_PATH, scope=SCOPES,
+      message=tools.message_if_missing(CLIENT_SECRETS_PATH))
+
+  # Prepare credentials, and authorize HTTP object with them.
+  # If the credentials don't exist or are invalid run through the native client
+  # flow. The Storage object will ensure that if successful the good
+  # credentials will get written back to a file.
+  storage = file.Storage('analyticsreporting.dat')
+  credentials = storage.get()
+  if credentials is None or credentials.invalid:
+    credentials = tools.run_flow(flow, storage, flags)
+  http = credentials.authorize(http=httplib2.Http())
+
+  # Build the service object.
+  analytics = build('analytics', 'v4', http=http, discoveryServiceUrl=DISCOVERY_URI)
+
+  return analytics
+
+def get_report(analytics):
+  # Use the Analytics Service Object to query the Analytics Reporting API V4.
+  return analytics.reports().batchGet(
+      body={
+        'reportRequests': [
+        {
+          'viewId': VIEW_ID,
+          'dateRanges': [{'startDate': '7daysAgo', 'endDate': 'today'}],
+          'metrics': [{'expression': 'ga:sessions'}]
+        }]
+      }
+  ).execute()
+
+
+def print_response(response):
+  """Parses and prints the Analytics Reporting API V4 response"""
+
+  for report in response.get('reports', []):
+    columnHeader = report.get('columnHeader', {})
+    dimensionHeaders = columnHeader.get('dimensions', [])
+    metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
+    rows = report.get('data', {}).get('rows', [])
+
+    for row in rows:
+      dimensions = row.get('dimensions', [])
+      dateRangeValues = row.get('metrics', [])
+
+      for header, dimension in zip(dimensionHeaders, dimensions):
+        print header + ': ' + dimension
+
+      for i, values in enumerate(dateRangeValues):
+        print 'Date range (' + str(i) + ')'
+        for metricHeader, value in zip(metricHeaders, values.get('values')):
+          print metricHeader.get('name') + ': ' + value
+
+
+def main():
+
+  analytics = initialize_analyticsreporting()
+  response = get_report(analytics)
+  print_response(response)
+
+if __name__ == '__main__':
+  main()
