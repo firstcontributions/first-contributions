c889be77439ed36e41b2688b6b361285f67fa598
riyagoyal17 <35290551+riyagoyal17@users.noreply.github.com>
2018-10-30 18:53:22 +0530

    Create pattern.c

---
+++ pattern.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ int main()
+{
+     int i , j ;
+    
+    for( i = 0 ; i < 5 ; i++)
+    {
+        for((j = 0 ; j <= i ; j++)
+        {
+        printf("*");
+        }
+    }
+    
+    
+    return 0;
+}
