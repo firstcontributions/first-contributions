7829ece9575151cdb243958bd59cf428324e9fac
suporte <augustojunior78@gmail.com>
2018-10-30 03:27:47 -0300

    check time of the day and show word

---
+++ checktime.php
@@ -0,0 +1,12 @@
+<?php
+$t = date("H");
+echo "<p>The hour (of the server) is " . $t; 
+echo ", and will give the following message:</p>";
+
+if ($t < "10") {
+    echo "Have a good morning!";
+} elseif ($t < "20") {
+    echo "Have a good day!";
+} else {
+    echo "Have a good night!";
+}
