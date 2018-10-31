8836efdd179045078d59728be3fb8bc3e3ffa171
GauthamG2 <36467370+GauthamG2@users.noreply.github.com>
2018-10-31 15:37:28 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,9 @@
+class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World"); 
+    } 
+} 
