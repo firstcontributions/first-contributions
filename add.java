37baa97caeb2a7deed4c13fbb5eb84e5f7acf6f7
GauthamG2 <36467370+GauthamG2@users.noreply.github.com>
2018-10-31 15:40:56 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,10 @@
+public class AddTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
