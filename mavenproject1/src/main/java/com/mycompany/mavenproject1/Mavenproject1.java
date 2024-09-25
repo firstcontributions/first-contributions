903858f76c9d9529c5ad025a7a80400f35241d94
Eric <127010882+Dcrievn@users.noreply.github.com>
2024-09-25 15:36:56 -0500

    Part 3: Contribution Commit

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Eric
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Test!");
+    }
+}
