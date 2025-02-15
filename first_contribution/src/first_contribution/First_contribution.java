d02063a065441f03e1a9294b0746b19874c8acde
cboard <168048361+cboard9@users.noreply.github.com>
2025-02-14 20:16:50 -0500

    Add cooper board to contributors list

---
+++ first_contribution/src/first_contribution/First_contribution.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package first_contribution;
+
+/**
+ *
+ * @author cboard
+ */
+public class First_contribution {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
