0cede7d611a2ea984a82fcf111126d880b7deeb6
f.tahmid123@gmail.com <f.tahmid123@gmail.com>
2025-08-20 08:56:29 +0600

    added Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+
+public class Calculator {
+
+    // Method to add two numbers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method to subtract two numbers
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Method to multiply two numbers
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Method to divide two numbers
+    public double divide(int a, int b) {
+        return (double) a / b;
+    }
+
+}
