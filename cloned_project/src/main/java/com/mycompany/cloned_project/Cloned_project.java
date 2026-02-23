c691641135a65757bd139a443cc06904e77001d9
juantrejosmusic <jjtr30@gmail.com>
2026-02-22 23:29:39 -0600

    This is the first commit to the first contribution forked repository

---
+++ cloned_project/src/main/java/com/mycompany/cloned_project/Cloned_project.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cloned_project;
+
+/**
+ *
+ * @author juanjosetrejos
+ */
+public class Cloned_project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This is my first commit with the clone project");
+    }
+}
